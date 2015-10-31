13c93a38201f949717642524f1c9e355c0aaed09
Ogi Martinovic <ogimart@gmail.com>
2015-10-31 12:53:21 -0400

    Add org-mode

---
+++ config/orgmode_init.el
@@ -0,0 +1,9 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ORG MODE
+
+(require 'org)
+
+;; log time when task is done
+;;(setq org-log-done 'time)
+; (setq org-todo-keywords
+;       '((sequence "ON-DECK" "IN-PROGRESS" "IN-REVIEW" "IN-QA" "|" "DONE")))
